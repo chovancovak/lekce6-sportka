dc52e24179eecbab81364e9beb65d341261da0a6
Karolína Chovancová <karolina.chloubova@gmail.com>
2021-04-15 19:46:20 +0200

    dokončeno

--- main.js
+++ main.js
@@ -6,3 +6,67 @@
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+
+let osudi = [];
+
+for (let i = 1; i < 49; i++) {
+    osudi.push(i);
+}
+
+let tazenaCisla = [];
+let historieHer = [];
+let vyherniCisla = document.querySelector("#vyherni-cisla");
+
+function losOsudi() {
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length)
+        let vyherniCislo = osudi[vyherniIndex];
+    
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+
+        setTimeout(function(){
+            
+            historieHer.push(vyherniCislo);
+    
+            vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span>';
+        }, i * 2000);
+
+        console.log(i);
+        
+        
+        
+    }
+}
+
+
+
+/*
+function losTazenaCisla () {
+    for (let i = 0; i < tazenaCisla.length; i++) {
+        
+        setTimeout(function(){
+            console.log (tazenaCisla);
+        }, 2000);
+        
+        vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span>';
+        
+
+    }
+}
+*/
+
+let button = document.querySelector("#tlacitko");
+
+tlacitko.addEventListener('click', losujZnovu);
+
+function losujZnovu() {
+    console.log (historieHer);
+    tazenaCisla = [];
+    vyherniCisla.innerHTML = "";
+    losOsudi();
+    
+   
+}
+
